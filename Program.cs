6ee8fbdce4e3d7fc5ecf9598be3fffdf0e61a39e
aslamrifai <61960015+aslamrifai@users.noreply.github.com>
2020-04-11 21:00:26 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Aslam", 19);
+            person.GetNameAndAge();
+            Console.WriteLine();
+            Teacher teacher = new Teacher("Kamarudin", 35, "190302", "Pemrograman");
+            teacher.GetNameAndAge();
+            Console.WriteLine();
+            Student student = new Student("Aslam", 19, "19.11.2689", "rifai.aslam@students.amikom.ac.id");
+            student.GetNameAndAge();
+
+            Console.ReadKey();
+        }
+    }
+}
